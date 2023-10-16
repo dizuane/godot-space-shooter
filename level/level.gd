3ff0d1de999dd6a7ba7613691ca197b19dd20a2d
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-16 19:33:13 -0400

    Added explosion prefab

--- level/level.gd
+++ level/level.gd
@@ -8,3 +8,5 @@ func _ready():
 func _process(delta):
 	if Input.is_key_pressed(KEY_Q):
 		GameManager.load_main_scene()
+	if Input.is_key_pressed(KEY_E):
+		ObjectMaker.create_explosion(Vector2(100, 200))
