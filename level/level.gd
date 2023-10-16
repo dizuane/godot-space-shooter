7050e09b210423ddaebf9baf29987165206335aa
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-16 19:38:00 -0400

    Added alternate explosion (boom) scene

--- level/level.gd
+++ level/level.gd
@@ -10,3 +10,5 @@ func _process(delta):
 		GameManager.load_main_scene()
 	if Input.is_key_pressed(KEY_E):
 		ObjectMaker.create_explosion(Vector2(100, 200))
+	if Input.is_key_pressed(KEY_B):
+		ObjectMaker.create_boom(Vector2(300, 150))
