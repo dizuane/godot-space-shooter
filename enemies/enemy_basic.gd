051136375f0b214018da920461386994815d9ea9
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-17 12:39:40 -0400

    Added booms on enemy death

--- enemies/enemy_basic.gd
+++ enemies/enemy_basic.gd
@@ -68,6 +68,20 @@ func shoot() -> void:
 	start_shoot_timer()
 
 
+func make_booms() -> void:
+	for b in booms.get_children():
+		ObjectMaker.create_boom(b.global_position)
+
+
+func die() -> void:
+	if _dead:
+		return
+	
+	_dead = true
+	set_process(false)
+	make_booms()
+	queue_free()
+
 func _on_laser_timer_timeout():
 	shoot()
 
@@ -87,5 +101,4 @@ func _on_area_2d_area_entered(area):
 
 
 func _on_health_bar_died():
-	set_process(false)
-	queue_free()
+	die()
