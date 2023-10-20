ca7c8f20c12b89e3b4d6db510a384c268ebfca78
Duane Spainhour <duane.spainhour@gmail.com>
2023-10-20 16:20:17 -0400

    Added shield power up logic

--- level/level.gd
+++ level/level.gd
@@ -8,3 +8,6 @@ func _ready():
 func _process(delta):
 	if Input.is_key_pressed(KEY_Q):
 		GameManager.load_main_scene()
+	if Input.is_action_just_pressed("test"):
+		ObjectMaker.create_power_up(Vector2(200,200),
+			GameData.POWERUP_TYPE.SHIELD)
